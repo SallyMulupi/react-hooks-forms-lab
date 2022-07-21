aab289f44477cf05a0cb08daf971e0baa4266d1b
SallyMulupi <mulupisally2gmail.com>
2022-07-22 02:39:16 +0300

    done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,13 +4,22 @@ import { v4 as uuid } from "uuid";
 
 
 function ItemForm({onItemFormSubmit} ) {
-  const [input,setInput]=useState({
-    id:null,
-    name:"",
-    category:"Produce"
-  });
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: itemName,
+    category: itemCategory,
+  };
   const handleChange=(event)=>{
+    const {name,value}=event.target;
+    setInput({
+      ...input,id:uuid(),[name]:value
+    });
 
+  }
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    onItemFormSubmit(input);
+  }
   return (
     <form className="NewItem">
       <label>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -27,6 +27,41 @@ function ShoppingList({ items }) {
       </ul>
     </div>
   );
+}function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search,setSearch]=useState("");
+  const [list, setList] = useState(items);
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All" && search === "") return true;
+
+    return item.category === selectedCategory || item.name.indexOf(search)> -1;
+  });
+  const handleSearchChange=(event)=>{
+    setSearch(event.target.value);
+
+  }
+  function updateList(newInput){
+    setList([...list, newInput])
+}
+function addElement(element) {
+  setArray([...array, element]);
+}
+  return (
+    <div className="ShoppingList">
+      <ItemForm onItemFormSubmit={updateList}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={search}/>
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ShoppingList;
